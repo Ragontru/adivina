ec0af8831af1b95f07d3fc7d8ce0ae1dcca01cdd
Raquel <ragontru25@gmail.com>
2021-03-15 10:21:09 +0100

    Práctica 1.4 completa

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,38 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  // Variables
+  num: number;
+  mayorMenor = '...';
+    // Se le asigna la respuesta de la funcion
+  numSecret: number = this.numAleatorio(0, 100);
 
+  constructor() { }
+
+  // Funcion que devuelve un numero aleatorio entre los dos numeros pasados por parametro
+  numAleatorio(a, b) {
+    return Math.round(Math.random() * (b - a) + parseInt(a, 10));
+  }
+
+  // Funcion que compara el numSecret con la variable num introducida y devuelve un mensaje
+  compruebaNumero() {
+    if (this.num) {
+      if (this.numSecret < this.num) {
+        this.mayorMenor = 'menor que';
+      }
+      else if (this.numSecret > this.num) {
+        this.mayorMenor = 'mayor que';
+      }
+      else {
+        this.mayorMenor = '';
+      }
+    }
+  }
+
+  reinicia() {
+    // reiniciamos las variables
+    this.num = null;
+    this.mayorMenor = '...';
+    this.numSecret = this.numAleatorio(0, 100);
+  }
 }
